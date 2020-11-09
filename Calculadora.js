08139ec3b75e8f5b119f20e90849bdffc0b9487d
André Luz <andrereisdaluz@hotmail.com>
2020-11-09 10:05:40 -0300

    :sparkles: Adiciona a função soma

--- Calculadora.js
+++ Calculadora.js
@@ -1,7 +1,11 @@
+var prompt = require('prompt-sync')()
 function sum(){
     //TODO: Implementar soma
+    var x = Number(prompt('Digite um número: '))
+    var y = Number(prompt('Digite outro número: '))
+    var soma = x + y
+    console.log(`A soma desses números é: ${soma}`)
 }
-
 function sub(){
     //TODO: Implementar subtração
 }
