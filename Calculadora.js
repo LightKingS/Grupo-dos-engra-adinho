4faad8d11f82c97930cdcaed36d15ce04212f913
André Luz <andrereisdaluz@hotmail.com>
2020-11-09 10:55:40 -0300

    :art: Adiciona divisão e retira comentários

--- Calculadora.js
+++ Calculadora.js
@@ -4,18 +4,15 @@ let a = Number(prompt('Digite um número: '))
 let b = Number(prompt('Digite outro número: '))
 
 function sum(a, b){
-    //TODO: Implementar soma
     return a + b;
 }
 function sub(){
-    //TODO: Implementar subtração
 }
 
 function mult(a, b){
-    //TODO: Implementar multiplicação
     return a * b;
 }
 
-function div(){
-    //TODO: Implementar divisão
+function div(a, b){
+    return a / b
 }
